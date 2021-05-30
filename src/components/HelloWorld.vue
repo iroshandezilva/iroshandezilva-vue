96027f31d899f68f1e5e969b4f9a4be9344be229
hashaaan <shalithavidanapathirana@gmail.com>
2021-05-30 15:08:35 -0700

    Add tailwing config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,25 +9,33 @@
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <button class="count-btn" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
+  <p>A portfolio for Iroshan De Zilva</p>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive } from "vue";
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: 0 });
 </script>
 
 <style scoped>
 a {
   color: #42b983;
+}
+.count-btn {
+  border: 1px solid #42b983;
+  padding: 5px 8px;
+  border-radius: 6px;
+}
+.count-btn:focus,
+.count-btn:active {
+  outline: unset;
 }
 </style>
